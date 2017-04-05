9fc4c83fc6162e5a30554fa0072f554c851c47eb
Henrik Jonsson <me@hkjn.me>
2017-04-05 20:39:59 +0200

    Upgrade to 0.9 by adding backends.tf, dropping remote_config.sh

--- README.md
+++ README.md
@@ -10,11 +10,3 @@ A dockerized Terraform alias `tf` can be added by:
 ```
 source tf_dockerized.sh
 ```
-
-## Remote state
-
-The project uses GCP to store state remotely:
-
-```
-bash remote_config.sh
-```
--- remote_config.sh
+++
@@ -1,6 +0,0 @@
-source tf_dockerized.sh
-run_tf remote config \
-       -backend=gcs \
-       -backend-config="bucket=hkjn-terraform-state" \
-       -backend-config="path=hkjninfra/prod/terraform.tfstate" \
-       -backend-config="project=henrik-jonsson"
