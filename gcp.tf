1ebeff7ea0beb24179f849a1f2c3eb8ee1ddbc33
Henrik Jonsson <me@hkjn.me>
2017-07-30 15:27:34 +0200

    Add iosdev.hkjn.me

--- gcp.tf
+++ gcp.tf
@@ -88,6 +88,18 @@ resource "google_dns_record_set" "hkjn_mon" {
   ]
 }
 
+resource "google_dns_record_set" "hkjn_iosdev" {
+  name = "iosdev.${google_dns_managed_zone.hkjn_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.hkjn_zone.name}"
+
+  rrdatas = [
+    "${var.iosdev_ip}",
+  ]
+}
+
 resource "google_dns_record_set" "hkjn_vpn" {
   name = "vpn.${google_dns_managed_zone.hkjn_zone.dns_name}"
   type = "A"
