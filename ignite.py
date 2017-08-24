7661419e0f31e26ab57c3619ab58afea12a56553
Henrik Jonsson <me@hkjn.me>
2017-08-24 04:35:57 +0000

    ignite.py: Refactor out more shared files and units

--- ignite.py
+++ ignite.py
@@ -14,7 +14,7 @@ def get_shared_files():
     """Return Ignition config for the shared files for all instances.
     
     Returns:
-        List of dict.
+        List of dict of files.
     """
     return [{
         'filesystem': 'root',
@@ -28,12 +28,50 @@ def get_shared_files():
             'mode': 493,
             'user': {},
             'group': {},
+        }, {
+            'filesystem': "root",
+            'path': '/opt/bin/report_client',
+            'contents': {
+                'source': 'https://github.com/hkjn/junk/releases/download/1.5.10/report_client_x86_64',
+                'verification': {
+                    'hash': 'sha512-f8eae52ca28902ef2f675378143464f7e0e4847066d2b2cc3170bb758819ede4aad8a4a641be1037cb924812de88f5ef0eb6db46a69810cd3dcf0c3ced6f4f08',
+                },
+            },
+            'mode': 493,
+            'user': {},
+            'group': {},
+        },
+    ]
+
+
+def get_shared_units():
+    """Return Ignition config for the shared systemd units for all instances.
+    
+    Returns:
+        List of dict of systemd units.
+    """
+    return [
+        {
+            'name': 'report_client.service',
+            'enable': True,
+            'contents': '[Unit]\nDescription=report client\nAfter=network-online.target\n\n[Service]\nEnvironment=PATH=/usr/bin/:/opt/bin:/bin\nEnvironment=REPORT_ADDR=mon.hkjn.me:50051\nEnvironment=REPORT_NAME=%H\nEnvironment=REPORT_FACTS_PATH=/etc/report_facts.json\nExecStartPre=-/bin/bash -c \"gather_facts > /etc/report_facts.json\"\nExecStart=/bin/bash -c report_client\n\n[Install]\nWantedBy=multi-user.target\n',
+        }, {
+            'name': 'report_client.timer',
+            'enable': True,
+            'contents': '[Unit]\nDescription=Timer that starts report_client.service\n\n[Timer]\n# Run every 5 min.\nOnCalendar=*:0/5\nPersistent=true\n\n[Install]\nWantedBy=multi-user.target\n',
         },
     ]
 
 
 def get_config(instance):
+    """Returns Ignition config for the instance.
+    
+    Returns:
+        Dict with Ignition config.
+    """
+
     shared_files = get_shared_files()
+    shared_units = get_shared_units()
     if instance == 'zg1':
         return {
             "ignition": {
@@ -42,19 +80,6 @@ def get_config(instance):
             },
             "storage": {
                 "files": shared_files + [{
-                                                        "filesystem": "root",
-                                                                "path": "/opt/bin/report_client",
-                                                                        "contents": {
-                                                                                      "source": "https://github.com/hkjn/junk/releases/download/1.5.10/report_client_x86_64",
-                                                                                                "verification": {
-                                                                                                                "hash": "sha512-f8eae52ca28902ef2f675378143464f7e0e4847066d2b2cc3170bb758819ede4aad8a4a641be1037cb924812de88f5ef0eb6db46a69810cd3dcf0c3ced6f4f08"
-                                                                                                                          }
-                                                                                                        },
-                                                                                "mode": 493,
-                                                                                        "user": {},
-                                                                                                "group": {}
-                                                                                                      },
-                                                  {
                                                               "filesystem": "root",
                                                                       "path": "/etc/coreos/update.conf",
                                                                               "contents": {
@@ -68,18 +93,13 @@ def get_config(instance):
                                                       ]
                               },
                       "systemd": {
-                              "units": [
+                              "units": shared_units + [
                                         {
                                                     "name": "bitcoin.service",
                                                             "enable": True,
                                                                     "contents": "[Unit]\nDescription=bitcoind\nAfter=network-online.target\n\n[Service]\nExecStartPre=-/bin/bash -c \"docker pull hkjn/bitcoin:$(uname -m)\"\nExecStartPre=-/usr/bin/docker stop bitcoin\nExecStartPre=-/usr/bin/docker rm bitcoin\nExecStart=/bin/bash -c \" \\\n  docker run --name bitcoin \\\n             -p 8333:8333 \\\n             --memory=1050m \\\n             --cpu-shares=128 \\\n             -v /containers/bitcoin:/home/bitcoin/.bitcoin \\\n             hkjn/bitcoin:$(uname -m)\"\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n"
                                                                           },
                                               {
-                                                          "name": "report_client.service",
-                                                                  "enable": True,
-                                                                          "contents": "[Unit]\nDescription=report client\nAfter=network-online.target\n\n[Service]\nEnvironment=PATH=/usr/bin/:/opt/bin:/bin\nEnvironment=REPORT_ADDR=mon.hkjn.me:50051\nEnvironment=REPORT_NAME=%H\nEnvironment=REPORT_FACTS_PATH=/etc/report/facts.json\nEnvironment=VERSION=1.5.7\nExecStartPre=/usr/bin/mkdir -p /etc/report\nExecStartPre=-/bin/bash -c \"gather_facts > /etc/report/facts.json\"\nExecStart=/bin/bash -c report_client\n\n[Install]\nWantedBy=network-online.target\n"
-                                                                                },
-                                                    {
                                                                 "name": "containers.mount",
                                                                         "enable": True,
                                                                                 "contents": "[Mount]\nWhat=/dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1-part1\nWhere=/containers\nType=xfs\n\n[Install]\nRequiredBy=local-fs.target\n"
@@ -101,24 +121,11 @@ def get_config(instance):
                 }],
                 "files": shared_files + [{
                         "filesystem": "root",
-                        "path": "/opt/bin/report_client",
-                        "contents": {
-                            "source": "https://github.com/hkjn/junk/releases/download/1.5.10/report_client_x86_64",
-                            "verification": {
-                                "hash": "sha512-f8eae52ca28902ef2f675378143464f7e0e4847066d2b2cc3170bb758819ede4aad8a4a641be1037cb924812de88f5ef0eb6db46a69810cd3dcf0c3ced6f4f08"
-                                }
-                            },
-                        "mode": 493,
-                        "user": {},
-                        "group": {}
-                        },
-                    {
-                        "filesystem": "root",
                         "path": "/opt/bin/decenter_world",
                         "contents": {
-                            "source": "https://github.com/hkjn/decenter.world/releases/download/1.1.1/decenter_world_x86_64",
+                            "source": "https://github.com/hkjn/decenter.world/releases/download/1.1.2/decenter_world_x86_64",
                             "verification": {
-                                "hash": "sha512-720604e3d5e594076f9ff2416a9a56b3354dc5255c0b42fe8de8aeb268b39eda80380f466b4834ab8812888995d83b96ff76948a90f7022d0ec0ef3bee4a2965"
+                                'hash': 'sha512-ed0fa9f29b504fb30ce7c33afc743e636bccffa6a9bd5630f9fd374cf6076725e6d44d8e2b11ed82f849de90cf009199bf2f19aa803ffd1830ddd75a837aeb06',
                                 }
                             },
                         "mode": 493,
@@ -150,18 +157,9 @@ def get_config(instance):
                             "group": {}
                             }
                     ]
-
         },
         'systemd': {
-                "units": [{
-                    "name": "report_client.service",
-                    "enable": True,
-                    "contents": "[Unit]\nDescription=report client\nAfter=network-online.target\n\n[Service]\nEnvironment=PATH=/usr/bin/:/opt/bin:/bin\nEnvironment=REPORT_ADDR=mon.hkjn.me:50051\nEnvironment=REPORT_NAME=%H\nEnvironment=REPORT_FACTS_PATH=/etc/report_facts.json\nExecStartPre=-/bin/bash -c \"gather_facts > /etc/report_facts.json\"\nExecStart=/bin/bash -c report_client\n\n[Install]\nWantedBy=multi-user.target\n"
-                    }, {
-                        "name": "report_client.timer",
-                        "enable": True,
-                        "contents": "[Unit]\nDescription=Timer that starts report_client.service\n\n[Timer]\n# Run every 5 min.\nOnCalendar=*:0/5\nPersistent=true\n\n[Install]\nWantedBy=multi-user.target\n"
-                        }, {
+                "units": shared_units + [{
                             "name": "decenter.service",
                             "enable": True,
                             "contents": "[Unit]\nDescription=decenter.world server\nAfter=network-online.target\n\n[Service]\nEnvironment=PATH=/usr/bin/:/opt/bin:/bin\nEnvironment=DECENTER_WORLD_ADDR=:443\nExecStart=/usr/bin/bash -c \"decenter_world\"\nRestart=always\n\n[Install]\nWantedBy=multi-user.target\n"
