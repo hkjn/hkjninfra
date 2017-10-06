2784b0f38d498001407cc428afb4cde8657131ca
Henrik Jonsson <me@hkjn.me>
2017-10-06 13:55:00 +0000

    Update tests

--- ignite_test.py
+++ ignite_test.py
@@ -9,6 +9,8 @@ class IgniteTest(unittest.TestCase):
     def test_get_config(self):
         self.maxDiff = None
         checksums = {
+            'decenter_world_x86_64': 'ce1ebb0b0192b61a6d94eba19db94d8fc4dbb72e8188ac69cc1c88e209f68ae9c57d32b07287abbc4149439ae41ba1f519bd4360dc3b8c7add47f65a52d33dab',
+            'decenter_redirector_x86_64': '735b7d08a457918eadfc049cc0063466756a7684bb5bd7cc660afcc4660a87d8685fca361e4b01678a0f01aea79f6831b9f1ca1c184bd44a7a2a63cb2b534d7c',
             'gather_facts': '55bb96874add4d200274cf1796c622da8e92244ad5b5fa15818bc516c5ed249e9cd98a736d44b66c7e03ca2b52e5aa898717fbd7d08ff13cd94de38ba2aef8c8',
             'tclient_x86_64': 'bf080645783c999f1a2bc8bc306660df8dbf496c6b7f98cf1d257d43c544050a7f4b6d1d9ba962c1d45fae8eb373061d3350e191edd73f1b44f01fc01448177f',
         }
@@ -49,7 +51,7 @@ class IgniteTest(unittest.TestCase):
                         'filesystem': 'root',
                         'group': {},
                         'mode': 493,
-                        'path': '/opt/bin/report_client',
+                        'path': '/opt/bin/tclient',
                         'user': {
                         },
                     }],
@@ -60,13 +62,28 @@ class IgniteTest(unittest.TestCase):
                     'passwd': {},
                     'units': [
                         {
-                            'contents': '[Unit]\nDescription=report client\nAfter=network-online.target\n\n[Service]\nEnvironment=PATH=/usr/bin/:/opt/bin:/bin\nEnvironment=REPORT_ADDR=mon.hkjn.me:50051\nEnvironment=REPORT_FACTS_PATH=/etc/report_facts.json\nExecStartPre=-/bin/bash -c "gather_facts > /etc/report_facts.json"\nExecStart=/bin/bash -c report_client\n\n[Install]\nWantedBy=multi-user.target\n\n',
+                            'contents': '\n'.join([
+                                '[Unit]',
+                                'Description=tclient',
+                                'After=network-online.target',
+                                '',
+                                '[Service]',
+                                'Environment=PATH=/usr/bin/:/opt/bin:/bin',
+                                'Environment=REPORT_ADDR=mon.hkjn.me:50051',
+                                'Environment=REPORT_FACTS_PATH=/etc/report_facts.json',
+                                'ExecStartPre=-/bin/bash -c "gather_facts > /etc/report_facts.json"',
+                                'ExecStart=/bin/bash -c tclient',
+                                '',
+                                '[Install]',
+                                'WantedBy=multi-user.target',
+                                '',
+                            ]),
                             'enable': True,
-                            'name': 'report_client.service',
+                            'name': 'tclient.service',
                         }, {
                             'contents': '[Unit]\nDescription=Timer that starts report_client.service\n\n[Timer]\n# Run every 5 min.\nOnCalendar=*:0/5\nPersistent=true\n\n[Install]\nWantedBy=multi-user.target\n',
                             'enable': True,
-                            'name': 'report_client.timer',
+                            'name': 'tclient.timer',
                         }, {
                             'dropins': [
                                 {
