9dab3ba072f7bb618a46e8304f0753122419969c
Henrik Jonsson <me@hkjn.me>
2017-10-06 13:25:45 +0000

    Add ignite_diff command

--- tf_dockerized.sh
+++ tf_dockerized.sh
@@ -1,3 +1,17 @@
+ignite_diff() {
+	local target
+	target=${1}
+	if [[ ! -e bootstrap/bootstrap_${target}.json ]]; then
+		echo "Unknown target ${target}." >&2
+		return 1
+	fi
+	run_tf output hkjn_ignite_json_${target} > /tmp/${target}_output.json
+	if [[ $? -ne 0 ]]; then
+		echo "tf output command failed: $(cat /tmp/${target}_output.json)" >&2
+	fi
+	diff <(jq '.' < /tmp/${target}_output.json) <(jq '.' < bootstrap/bootstrap_${target}.json)
+}
+
 run_tf() {
 	local action
 	action=$1
@@ -11,13 +25,9 @@ run_tf() {
 	# TODO: Below we take current VERSION file, but could run an older version
 	# for some targets, as specified in ignition.py..
 	echo "version = \"$(cat VERSION)\"" > terraform.tfvars
-	# TODO: Could store user-data fields with 'tf output', then
-	# diff the before / after and show better diffs:
-	# 1. tf output hkjn_ignite_json_zg1 > zg1.json
-	# 2. diff <(jq '.' < zg1.json) <(jq '.' < bootstrap/bootstrap_zg1.json)
-	# By doing 'tf plan -detailed-exitcode', we can check for status 2 (there was
-	# a diff), and if so, run the diff command above for a not-horrible comparison
-	# on where the metadata differs
+	# TODO: By doing 'tf plan -detailed-exitcode', we can check for status 2 (there was
+	# a diff), and if so, run the ignite_diff command above for a not-horrible comparison
+	# on where the metadata differs.
 	docker run --rm -it -v $(pwd):/home/tfuser \
 	           -e GOOGLE_APPLICATION_CREDENTIALS=/home/tfuser/.gcp/tf-dns-editor.json \
 		   hkjn/terraform $@
