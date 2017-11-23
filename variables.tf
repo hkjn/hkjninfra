af11e836c25dd65336393cb0be55e354c3c36e94
Henrik Jonsson <me@hkjn.me>
2017-11-23 17:44:49 +0000

    Remove unused key

--- variables.tf
+++ variables.tf
@@ -44,10 +44,6 @@ variable "zg0_pubkey" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCw76RMWPriShiqQKy06oJjlYURKdwStODyUsgwBIEdf/5Vzs8CDCVN9MtML1F+D7p3HsqUMLuMaW8516dI/KgRXVEkNJPTBGGPe0sxYDztDBDYeCxNgvOzCT/A39LPULHM1tUE84+Q33/yVSKhHl5Bzg+WU02ksN3DNQijs3OfYZ1E6ltnutmIgS0qnFFh54EUHhUg+p3T0sUM7RaB6sfJWU1JqsKNrFvK4+LjSjjvRfLB9eB+jckk92DverhOn/4fcbToeDU6fV+11X86EvqKpSD4wQrxvtT5iszYxDFPngeESDs/4c+n3qj+BXz2dvK6BUzseOeV/cpRkP3fwJy6j9Uklw2Y22JR9lLuhZWJdZR8dsY01zs7f0N3y+urmgOWDD5WHoJeqDR7NOU9NNXEgC/b6/5EhNfhLYCpuw4YQf7V3AGR6qFSj5R0Jjye8hZ6i74NshmLoaszFHCZX5XI7kmzI9BJCPvf1ZIdHCst+mh8yMRygJ3LvBuMkRrzENFpfTPXtTbdu1pJTPOa1JGpqSJwaM4MfkRO9sh/ddtT0ADtSkWwcNekUvrTHEs6W5IYUm3O0EhFU39m+KOedD+rv7I5uK7RqV4HRPXb4ewE1b3+SpJ44EujlkPTBu38AVI0UV24jEOTHNJprLSgV7tYLwzZLBetvXeuQJc5mfNYSQ== zero@gz0"
 }
 
-variable "gz1_pubkey" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDhkGPTKaQxOkrRO4gyJFr5ewcIHVcQ+YyEeTWfK1GpwKwjMZNhbXCL0DeTxjYx+JHMqStxlKi6H/+dDJSiohbIi1MqPcpPvaY2whLPSHrV4b17tYXjnkZbowDp54QCTLgcXmfckpKosulV56t6GGXZ2PmlP85QkpyeF+8/EyS+8XFQi1GeWOwgnBvmyRRxSBwsJnMt+aJdCcIRv0sO+pKuzCJFLJ2D82sRbfLLIWOy0BkzaUMevO+sUYmmRgJ2Kis7MFNYC8c6i2GHTuRex5t0fHyCzp7qHLISAhjS4d5mmpfUeMJot7WlOiwlVTMcoQSFY0boTuDSEoVxzwu3/R2zc6Hu+RGBKZbIR6ksKZFPe64dPGJXzfX/3oyqhFQKF3A9kl9wNGq2RQweEaRtHmvToAf49iN+8q5vMdSD0XbCxJwakCj+nKLprp7yexydj6JX+lDbXtxJyF5ECuN4mE0Mi09dFJSS4e09c9bCSrIM+6AXOLpU10Kq2KqAiRxRpdV5UxheHeK89E6Z4Y8d1DggmrnQxSfvmfCctqsJVWuXC2yl+py8PmP1I9HclyBwCReTVUpOOJknQILh1lubAtboBbBK9zAIh8k13224ricxSDhiP4T+Jl6tguzxK7hQ1zQJlRipyTtaktSSikA2aMobmHo2IoifRkblJ2Wc6BiOOQ== gz1"
-}
-
 variable "gcloud_project" {
   default = "henrik-jonsson"
 }
@@ -103,8 +99,6 @@ variable "cities_ip" {
 variable "builder_enabled" {
   description = "Whether builder node is enabled."
   default = false
-  # TODO: Create alpine package with grpc instead of compiling from source in hkjn/workspace
-  # builds.
 }
 
 variable "dropcore_enabled" {
